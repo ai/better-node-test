9f49a7fac347a6c5a541902444e7038a234ae737
Andrey Sitnik <andrey@sitnik.ru>
2024-06-25 21:23:23 +0000

    Update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,10 @@
 import loguxConfig from '@logux/eslint-config'
 
-export default [...loguxConfig]
+export default [
+  ...loguxConfig,
+  {
+    rules: {
+      'n/no-unsupported-features/node-builtins': 'off'
+    }
+  }
+]
