ff8d2b96f53398c14a472d0e753a31306c6ef659
Andrey Sitnik <andrey@sitnik.ru>
2025-10-15 16:19:25 +0000

    Fix old Node.js test

--- test/find.js
+++ test/find.js
@@ -7,16 +7,18 @@ process.stdin.on('data', data => {
 })
 
 process.stdin.on('close', code => {
+  process.stdout.write(input)
   if (process.argv[3]) {
     let major = process.version.match(/v\d+\./)[1]
     if (process.argv[3].split(',').includes(major)) {
       if (code === 0) {
         process.stderr.write('No exit code 1 on non-supported Node.js')
         process.exit(1)
+      } else {
+        process.exit(0)
       }
     }
   }
-  process.stdout.write(input)
   if (!input.includes(process.argv[2])) {
     process.exit(1)
   }
